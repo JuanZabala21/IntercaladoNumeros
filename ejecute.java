79bb98918229ccc15ba1e9c7dfa81ff63f54fe84
Juan Zabala <44419062+JuanZabala21@users.noreply.github.com>
2019-05-05 21:12:11 -0400

    Update ejecute.java

--- ejecute.java
+++ ejecute.java
@@ -1,5 +1,5 @@
 package Intercalate;
-//Juan Zabala , 26.900.042
+//Juan Zabala
 public class RE_Zabala_26900042 {
 public static void main(String[] args) {
       Intercalado v = new Intercalado(); 
