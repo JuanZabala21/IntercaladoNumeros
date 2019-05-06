5c5d351c966fe8ea060b20670a816e831f2767fc
Juan Zabala <44419062+JuanZabala21@users.noreply.github.com>
2019-05-05 21:10:47 -0400

    Create ejecute.java

---
+++ ejecute.java
@@ -0,0 +1,9 @@
+package Intercalate;
+//Juan Zabala , 26.900.042
+public class RE_Zabala_26900042 {
+public static void main(String[] args) {
+      Intercalado v = new Intercalado(); 
+      v.setVisible(true); 
+    }
+    
+}
